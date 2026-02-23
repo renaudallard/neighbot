658419493b09ea856e8e8516b6c90634bbbc75be
Renaud Allard <renaud@allard.it>
2026-02-23 17:49:01 +0100

    Bump version to 0.1.4

--- neighbot.h
+++ neighbot.h
@@ -23,7 +23,7 @@
 #include <stdint.h>
 #include <time.h>
 
-#define NEIGHBOT_VERSION "0.1.3"
+#define NEIGHBOT_VERSION "0.1.4"
 
 #define DEFAULT_DBFILE  "/var/neighbot/neighbot.csv"
 #define DEFAULT_MAILTO  "root"
